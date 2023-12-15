cc0f81a17a1a4600c8a9ff97c62534c868be864f
sonah5009 <sonah5009@gmail.com>
2023-12-15 22:44:44 +0900

    python 수정

--- api/index.py
+++ api/index.py
@@ -10,7 +10,8 @@ app = FastAPI()
 # CORS 미들웨어 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # 프론트엔드 서버 URL
+    allow_origins=["http://localhost:3000",
+                   "https://nextjs-fastapi-ilycmrtzr-sonah5009.vercel.app",],  # 프론트엔드 서버 URL
     allow_credentials=True,
     allow_methods=["*"],  # 모든 HTTP 메서드 허용
     allow_headers=["*"],  # 모든 HTTP 헤더 허용
--- next.config.js
+++ next.config.js
@@ -23,6 +23,48 @@ const nextConfig = {
             ? "http://127.0.0.1:8000/openapi.json"
             : "/api/openapi.json",
       },
+      {
+        source: "/friends",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/friends"
+            : "/api/",
+      },
+      {
+        source: "/friend-name",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/friend-name"
+            : "/api/",
+      },
+      {
+        source: "/chat-rooms",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/chat-rooms"
+            : "/api/",
+      },
+      {
+        source: "/messages",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/messages"
+            : "/api/",
+      },
+      {
+        source: "/create-or-get-personal-chat-room",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/create-or-get-personal-chat-room"
+            : "/api/",
+      },
+      {
+        source: "/insert-message",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/insert-message"
+            : "/api/",
+      },
     ];
   },
 };
