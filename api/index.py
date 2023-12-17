1328537a2d0f65329ab3b6c7b65697ee65fbb336
sonah5009 <sonah5009@gmail.com>
2023-12-17 16:56:54 +0900

    localhot 수정

--- api/index.py
+++ api/index.py
@@ -11,7 +11,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
-                   "https://nextjs-fastapi-ilycmrtzr-sonah5009.vercel.app",],  # 프론트엔드 서버 URL
+                   "https://nextjs-fastapi-ilycmrtzr-sonah5009.vercel.app",  # 프론트엔드 서버 URL
+                   "https://nextjs-fastapi-lilac.vercel.app"],  # 프론트엔드 서버 URL
     allow_credentials=True,
     allow_methods=["*"],  # 모든 HTTP 메서드 허용
     allow_headers=["*"],  # 모든 HTTP 헤더 허용
